4c15884f1aa3c6bfe56fc01401e11927f95e6293
Benjamin.M <44535859+MrSSHH@users.noreply.github.com>
2020-03-26 23:17:23 +0200

    Update
    * Download button working only on files
    * better code
    * Added logout button
    * added size section in explorer (bug working with a folder)

--- PyBackup/main.py
+++ PyBackup/main.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, send_from_directory, request, make_response
+from flask import Flask, render_template, redirect, url_for, request, Response
 from forms import LoginForm, ConsoleCommand
-from os import path, chdir, getcwd, listdir, remove
+from os import path, getcwd, listdir, remove, walk
 from shutil import rmtree
+import zipfile
 
 main_dir = path.join(getcwd(), 'upload_cloud')
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'af7e8e3e25d417f9a0635906dc0325be'
+
 
 @app.route("/", methods=["POST", "GET"])
 def login():
@@ -16,42 +18,75 @@ def login():
         if form.username.data == 'admin' and form.password.data == 'admin':
             return redirect(url_for("home"))
         else:
-            return render_template("login.html", title="Login", form=form, wrong='yes')
+            return render_template(
+                "login.html",
+                title="Login",
+                form=form,
+                wrong='yes')
 
     return render_template("login.html", title="Login", form=form, wrong='no')
 
 
 @app.route('/viewer/<filepath>')
 def view(filepath):
+    url = request.path
     file_open = open(filepath, 'r')
     file_content = file_open.read().replace('\n', '<br>')
     file_open.close()
 
-    return render_template('viewer.html', file_content=file_content, file_path=filepath, file_name=path.split(filepath)[1])
+    return render_template(
+        'viewer.html',
+        file_content=file_content,
+        file_path=filepath,
+        file_name=path.split(filepath)[1],
+        url=url)
 
 
 @app.route('/explorer', defaults={'dir': main_dir})
 @app.route('/explorer/<dir>')
 def explore(dir):
-    if not dir.startswith(main_dir):
+    url = request.path
+
+    if not dir.startswith(main_dir) or not request.referrer:
         return redirect(url_for('login'))
 
     files = []
-    folders = []
 
     for index, file in enumerate(listdir(dir)):
         full_path = path.join(dir, file)
-        if path.isfile(full_path):
-            files.append([index, file, full_path])
-        else:
-            folders.append([index, file, full_path])
 
-    return render_template('explorer.html', files=files, folders=folders, path_dir=dir, main_dir=main_dir)
+        size = int(path.getsize(full_path))
+
+        if size >= 1073741824:
+            size = size / 1073741824
+            size_type = 'GB'
+        elif size >= 1000000:
+            size = size / 1000000
+            size_type = 'MB'
+        elif size >= 1000:
+            size = size / 1000
+            size_type = 'KB'
+        else:
+            size_type = 'Bytes'
+
+        if path.isfile(full_path):
+            files.append([index, file, full_path, int(size), size_type, 'f'])
+            continue
+
+        files.append([index, file, full_path, size, size_type, 'o'])
+    return render_template(
+        'explorer.html',
+        files=files,
+        path_dir=dir,
+        main_dir=main_dir,
+        url=url)
 
 
 @app.route("/home")
 def home():
-    return render_template('index.html')
+    url = request.path
+    return render_template('index.html', url=url)
+
 
 @app.route('/delete/<file_path>')
 def delete(file_path):
@@ -62,46 +97,18 @@ def delete(file_path):
     return redirect('/explorer')
 
 
-@app.route("/download/<path><filename>")  # fix from upload to download
-def download(path, filename):
+@app.route("/download/<f_path>")
+def download(f_path):
+    file_name = path.split(f_path)[1]
+    file_ext, file_path = path.splitext(f_path)
+    with open(f_path, 'r') as file:
+        data = file.read()
+        return Response(
+            data,
+            mimetype="text/{}".format(file_ext),
+            headers={
+                "Content-disposition": "attachment; filename={}".format(file_name)})
 
-    return send_from_directory(getcwd(), filename=filename, as_attachment=True)
-
-
-@app.route("/serverconsole", methods=["POST", "GET"])
-def console():
-    if not request.referrer:
-        return redirect(url_for("login"))
-    else:
-        global file
-        form = ConsoleCommand()
-        data = ""
-        error = ''
-        commands = {"show files": listdir(getcwd()), "help": 'show files\ncd\nremove dir\ndownload'}
-
-        if form.validate_on_submit():
-            try:
-                command = str(form.console_command.data)
-                if 'cd' in command:
-                    chdir(path.join(getcwd(), command.split(' ')[1]))
-
-                elif "remove dir" in command:
-                    rmtree(path.join(getcwd(), command.split(" ")[2]))
-
-                elif "download" in command:
-                    file = command.split(" ")[1]
-                    return download(file)
-
-                elif command in commands.keys():
-                    data = commands[command]
-
-                else:
-                    error = "Command is not found"
-
-            except BaseException as v:
-                error = f'{v}'
-
-        return render_template("console.html", title="Console", data=data, form=form, error=error)
 
 if __name__ == "__main__":
     app.run(debug=True)
